71cbe8dcc3c1d1014598ba119f6a0468c191fdf5
Alain Achkar <rails@alain.simplytech.com>
2011-01-15 22:05:17 -0500

    Basic working version.

---
+++ jam_setup.bash
@@ -0,0 +1,18 @@
+#*****************************************************************************#
+# File   : jam_setup.bash                                                     #
+# Author : Alain Achkar                                                       #
+# Summary: Setup env vars for Jam                                             #
+#*****************************************************************************#
+#                                                                             #
+# Usage:                                                                      #
+#   (bash) source jam_setup.bash                                              #
+#                                                                             #
+#*****************************************************************************#
+
+   export TOP=.
+   export JAMRULES=rules.jam
+   export JAMFILE=makefile.jam
+
+   #export JAM_DEBUG=1
+   #export JAM_DEBUG_INCLUDED_FILES=1
+   
