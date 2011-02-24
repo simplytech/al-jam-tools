b376fae1d533a2a54e0d4ae8eac46bfcbd9bdf8e
Alain Achkar <rails@alain.simplytech.com>
2011-02-24 15:36:48 -0500

    Added capability to execute rails commands in the Jam actions

--- jam-setup.bash
+++ jam-setup.bash
@@ -16,6 +16,14 @@ if [ "$1" == "" ] ; then
   return
 fi
 
+if [ "$2" == "" ] ; then
+  echo Must specify RAILS_APP as arg2
+  return
+fi
+
+#Example:
+alias js="source ~/code/al-jam-tools/jam-setup.bash ~/rails_projects/rails3-unr photos"
+
 unset TOP
 unset JAMRULES
 unset JAMFILE
@@ -26,6 +34,7 @@ export TOP=~/code/al-jam-tools
 export JAMRULES=$TOP/rules.jam
 export JAMFILE=$TOP/makefile.jam
 export PROJECT_ROOT=$1
+export RAILS_APP=$2
 
 #export JAM_DEBUG=1
 #export JAM_DEBUG_INCLUDED_FILES=1
@@ -33,13 +42,14 @@ export PROJECT_ROOT=$1
 echo TOP=$TOP
 echo JAMRULES=$JAMRULES
 echo JAMFILE=$JAMFILE
+echo PROJECT_ROOT=$PROJECT_ROOT
+echo RAILS_APP=$RAILS_APP
 
 # To rename Boost Jam from bjam to jam, however, jam already exists, so I renamed it to al-jam.
 # The name should not contain 'bjam' because they do a wildcard search on argv[0] to determine
 # if we want to run jam or bjam
 
 alias j='al-jam' 
-alias js='. ~/code/al-jam-tools/jam-setup.bash'
 
 # Execute a step at Page <n>
 # Example:
@@ -48,15 +58,23 @@ alias js='. ~/code/al-jam-tools/jam-setup.bash'
 # will execute:
 #  rails new chapter-1
 
+# Execute commands on Page <n>
 p()
 {
   al-jam -o .my-cmd.tmp "$@" && \
   tail -n +2 .my-cmd.tmp > .my-cmd && \
   chmod +x .my-cmd && \
   ./.my-cmd
+}
+
+# Execute commands on Page <n>, and submit (i.e. commit)
+pp()
+{
+  p "$@"
   s "$@"
 }
 
+# Execute commands on Page <n> intercatively
 i()
 {
   p "$@"
@@ -73,7 +91,7 @@ s()
     git add .
     git commit -m "Page $@"
     git tag -f "$@"
-    git push origin master
+#    git push origin master
 
   cd $CUR_DIR
 }
